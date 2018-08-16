f56217066b5e23bf1605d3ac84f26a838f7b02d1
Nathan Zukoff <nzukoff@gmail.com>
2018-08-15 23:38:46 -0700

    Colors image based on a chosen number

--- pixo.py
+++ pixo.py
@@ -1,47 +1,46 @@
-from PIL import Image
-from collections import Counter
+from PIL import Image, ImageDraw
 from sklearn.cluster import KMeans
 import numpy as np
 
-im = Image.open('./mms.jpg') 
+im = Image.open('./worms.jpg') 
 pix = im.load()
 pix_val = np.array(list(im.getdata()))
 
-kmeans = KMeans(n_clusters = 12)
+kmeans = KMeans(n_clusters = 8)
 y_kmeans = kmeans.fit_predict(pix_val)
 
-# print(y_kmeans)
-
-for center in kmeans.cluster_centers_:  
+for idx, center in enumerate(kmeans.cluster_centers_):
     color = (int(center[0]), int(center[1]), int(center[2]))
-    # img = Image.new('RGB', (300, 200), color)
-    # img.show()
+    img = Image.new('RGB', (300, 200), color)
+    d = ImageDraw.Draw(img)
+    d.text((10,10), str(idx), fill=(0,0,0))
+    img.show()
 
-data = []
-for idx, label in enumerate(y_kmeans):
-    if label == 3 or label == 1 or label == 5:
-        data.append(tuple(pix_val[idx]))
-    else:
-        data.append((255,255,255))
+data = np.array([(255,255,255) for d in range(len(pix_val))])
+guesses = []
+while True:
+    chosen_number = int(input("Pick a number: "))
+    if chosen_number not in guesses:
+        chosen_indices = np.where(y_kmeans == chosen_number)
+        data[chosen_indices] = pix_val[chosen_indices]
+        tupled_data = [(d[0], d[1], d[2]) for d in data]
+        guesses.append(chosen_number)
+        image = Image.new("RGB", im.size)
+        image.putdata(tupled_data)
+        image.show()
+        print(guesses)
+    elif chosen_number in guesses:
+        print("ALREADY GUESSED THAT")
 
+## CREATES DATA FOR SPECIFIC LABELS
+# data = []
+# for idx, label in enumerate(y_kmeans):
+#     if label == 3 or label == 1 or label == 5:
+#         data.append(tuple(pix_val[idx]))
+#     else:
+#         data.append((255,255,255))
 
-# data = np.full(len(y_kmeans),255)
-# indices = np.where(y_kmeans == 7)
-# print(indices)
-# np.put(data, indices, pix_val[indices])
-
-# it = iter(data)
-# tupled = list(zip(it, it, it))
-
-# tupled1 = [(data[i], data[i+1], data[i+2]) for d,i in enumerate(data[::2])]
-# print("DATA ", data)
-# print("TUPLED IS ", len(list(tupled)))
-
-
-image = Image.new("RGB", im.size)
-image.putdata(data)
-image.show()
-
+## MOST COMMON COLORS
 # color_count = Counter()
 # for pix in pix_val:
 #     red = pix[0] >> 5
